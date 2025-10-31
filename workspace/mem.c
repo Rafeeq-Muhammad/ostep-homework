6de29b6fbb26e5f1b564b37659b06248ba246b93
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-10-31 16:54:23 -0700

    runnign

--- workspace/mem.c
+++ workspace/mem.c
@@ -1,12 +1,19 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "rafeeq.h"
 
 int main(int argc, char* argv[]) {
     int* p = malloc(sizeof(int));
     assert(p != NULL);
-    printf("%p\n", p);
+    printf("(%d) address of p: %08x\n", getpid(), (unsigned)p);
+    *p = 0;
+    while(1) {
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) address of p: %08x\n", getpid(), (unsigned)p);
+    }
     free(p);
     return 0;
 }
